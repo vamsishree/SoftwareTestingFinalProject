6023fd915fd785675ef06d369de5f193f4fd464b
vamsishree <vamsishree06@gmail.com>
2022-03-13 12:50:36 +0100

    Right test cases and right functionality....

--- src/main/java/DistanceConverter.java
+++ src/main/java/DistanceConverter.java
@@ -1,12 +1,10 @@
 public class DistanceConverter {
     public int kms_to_meters(int kms){
-        //return kms*1000;
-        return 0;
+        return kms*1000;
     }
     public int meters_to_kms(int meters){
-        //return meters/1000;
-        return 0;
+        return meters/1000;
     }
 
 }
-//pushing the code with wrong functionality and right test cases....
+
