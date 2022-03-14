1164611187df6d12f75e84bb60247128eeba44bb
Erick Martinez <57727572+erickmart23@users.noreply.github.com>
2022-03-14 01:30:24 +0100

    Test Cases with wrong functionalities

--- src/main/java/DistanceConverter.java
+++ src/main/java/DistanceConverter.java
@@ -8,15 +8,13 @@ public class DistanceConverter {
     public int cms_to_mms(int cms) {return cms*10;}
     public int mms_to_cms(int mms){ return mms/10; }
 
-    //Second Test Cases with wrong functionalities
-/*
     //Kilometre tests
     public double kms_to_miles(int kms) { return kms*0.621;}
 
     public double kms_to_foot(int kms) { return kms*3280.840; }
     public double kms_to_inch(int kms) { return kms*39370.1; }
-    public int kms_to_cms(int kms) { return kms * 100000; }
-    public int kms_to_mm(int kms) { return kms*1000000; }
+    public int kms_to_cms(int kms) { return kms * 10000; }
+    public int kms_to_mm(int kms) { return kms*100000; }
     //Meter tests
 
     public double meters_to_foot(int meters) {return meters*3.2;}
@@ -28,6 +26,6 @@ public class DistanceConverter {
 
     public double cms_to_foot(int cms) {return cms*0.032;}
     public double cms_to_inch(int cms) {return cms*0.393701;}
-*/
+
 //Third Test Cases with good functionalities
 }
